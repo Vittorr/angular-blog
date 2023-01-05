36dcae0c84ba0ca355a8e70d0e443949b184540d
Vittorr <paulobjalves@hotmail.com>
2023-01-05 18:28:23 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -11,6 +11,8 @@ export class SmallCardComponent implements OnInit {
   photoCover: string = "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
   @Input()
   cardTitle: string = "NOVA SERIE ANUNCIADA"
+  @Input()
+  Id: string = "0"
 
   constructor() { }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia novo homem de ferro",
+    "photoCover": "https://image.api.playstation.com/vulcan/img/rnd/202010/2716/5f11i65d9kLRGpvRCbxU6XGE.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Nova Série anunciada no Disney Plus",
+    "description": "bla bla bla",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+]
